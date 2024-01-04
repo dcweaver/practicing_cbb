629b3625506d85ce9103ec12c5fac3f15b4e38ce
dcweaver <50436656+dcweaver@users.noreply.github.com>
2024-01-04 13:12:11 -0600

    updated folder structure

---
+++ cbbplotR_learning.R
@@ -0,0 +1,9 @@
+#CBBPLOTR INITIAL PRACTICE
+install.packages("cbbplotR")
+devtools::install_github("andreweatherman/cbbplotR") 
+
+library(cbbdata)
+library(tidyverse)
+library(gt)
+library(devtools)
+library(cbbplotR)
